52858725519bf1007893ce310fbe58819e4d59fd
xennia316 <xennia316@gmail.com>
2022-01-26 20:43:55 +0100

    popping, shifting, pushing and unshifting in array

---
+++ FCC/Basic_JavaScript/Notes/array-manipulation.js
@@ -0,0 +1,13 @@
+
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+console.log(myArray);
+
+myArray.unshift(["Paul", 35])
+console.log(myArray);
+
+myArray.push(["Sonia", "Nice"]);
+console.log(myArray);
+
+myArray.pop();
+console.log(myArray);
