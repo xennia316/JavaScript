42e55eea19a02a2457af8c44706e66e91215360d
xennia316 <xennia316@gmail.com>
2022-01-26 20:39:55 +0100

    nested arrays to make shopping list

---
+++ FCC/Basic_JavaScript/Notes/shopping-list.js
@@ -0,0 +1,10 @@
+const myList = [
+  ["Chocolate Bar", 25],
+  ["MacBook Pro", 1],
+  ["iPhone 12Pro", 1],
+  ["Dresses", 1000],
+  ["Sneakers", 30],
+  ["Excesses", 200]
+];
+
+console.log(myList);
