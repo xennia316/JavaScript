1d41cb475d5655bc115967cb2678725984b7afec
xennia316 <xennia316@gmail.com>
2022-01-28 19:12:29 +0100

    strictly equal and simply equal

---
+++ FCC/Basic_JavaScript/Notes/equality.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {*} val 
+ * @returns 
+ */
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
