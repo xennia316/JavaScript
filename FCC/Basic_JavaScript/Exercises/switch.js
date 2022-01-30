69f7bdf5a137f1c177f134ed5a3cefe23ba2fed3
xennia316 <xennia316@gmail.com>
2022-01-30 21:31:20 +0100

    switching cases without a default

---
+++ FCC/Basic_JavaScript/Exercises/switch.js
@@ -0,0 +1,29 @@
+/**
+ * @xennia316
+ * @param {*} val 
+ * @returns 
+ */
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+     answer = "gamma";
+     break;
+    case 4:
+     answer = "delta";
+     break;
+  }
+
+console.log(answer);
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
