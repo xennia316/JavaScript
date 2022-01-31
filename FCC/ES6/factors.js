66602f8193630bbd59d6e6daa4a3c881206c9610
xennia316 <xennia316@gmail.com>
2022-01-31 20:00:34 +0100

    factors of a number

---
+++ FCC/ES6/factors.js
@@ -0,0 +1,13 @@
+/**
+ * @xennia316
+ * finding the factors of a numbers.
+ */
+let num1;
+let num2;
+
+function factor(num1) {
+    if (num1 % num2 == 0) {
+        return num2;
+    }
+}
+console.log(factor(4));
