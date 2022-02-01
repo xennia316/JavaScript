aa70fa544036c586822fb9eae44b2e65f82621fd
xennia316 <xennia316@gmail.com>
2022-02-01 20:45:13 +0100

    comments replaced

--- FCC/Basic_JavaScript/Notes/assignment1.js
+++ FCC/Basic_JavaScript/Notes/assignment1.js
@@ -1,11 +1,12 @@
-// Setup
+/**
+ * by @xennia316
+ */
 let processed = 0;
-
+//processed initialized to zero
 function processArg(num) {
   return (num + 3) / 5;
 }
-
-// Only change code below this line
+// takes number and adds three then divides by 5.
 processed = processArg(7);
 
 console.log(processed);
