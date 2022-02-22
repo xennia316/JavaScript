0dab0d45ad208f80c22015a4ecb29593970ea06e
xennia316 <xennia316@gmail.com>
2022-02-22 14:07:11 +0100

    manipulating complex objects

---
+++ FCC/Basic_JavaScript/Exercises/manipulating-omplex-objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Zendaya",
+    "title": "Model and Daancer",
+    "release_year": 2035,
+    "formats": [
+      "CD",
+      "LP",
+      "8T",
+      "DVD"
+    ],
+  }
+];
