a0c76e0783c59f4aa5de36e110944d003f91fc19
xennia316 <xennia316@gmail.com>
2022-05-03 12:11:35 +0100

    converting a string to a title

---
+++ FCC/BasicAlgorithmScripting/conversion.js
@@ -0,0 +1,12 @@
+function titleCase(str) {
+    const newArray = (str.split(' '));
+    console.log(newArray)
+    const updated = []
+    for (let st in newArray){
+          updated[st] = newArray[st][0].toUpperCase() + newArray[st].slice(1).toLowerCase();
+    }
+    console.log(updated.join(" "))
+    return updated.join(" ")
+  }
+  
+  titleCase("I'm a little tea pot");
