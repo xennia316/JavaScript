7e05a9f7be38d4644c20773da593573e4d32039a
xennia316 <xennia316@gmail.com>
2022-11-16 11:12:29 +0100

    undo changes

--- FCC/ES6/Exercises/divideArray.js
+++ FCC/ES6/Exercises/divideArray.js
@@ -1,19 +1,19 @@
 function chunkArrayInGroups(arr, size) {
-  let final = [];
+  let finale = [];
   let temp = [];
   for (let i = 0; i < arr.length; i++) {
     if (i % size !== size - 1) {
       temp.push(arr[i]);
     } else {
       temp.push(arr[i]);
-      final.push(temp);
+      finale.push(temp);
       temp = [];
     }
   }
   if (temp.length !== 0) {
-    final.push(temp);
+    finale.push(temp);
   }
-  return final;
+  return finale;
 }
 
 chunkArrayInGroups(["a", "b", "c", "d", "e", "f"], 2);
