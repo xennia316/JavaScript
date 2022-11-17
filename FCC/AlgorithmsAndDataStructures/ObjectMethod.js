0649ceca8eacbd75dabafe5899d1d353fb977675
xennia316 <xennia316@gmail.com>
2022-11-17 03:54:09 +0100

    correction

--- FCC/AlgorithmsAndDataStructures/ObjectMethod.js
+++ FCC/AlgorithmsAndDataStructures/ObjectMethod.js
@@ -2,7 +2,7 @@ let dog = {
   name: "Spot",
   numLegs: 4,
   sayLegs: function () {
-    return "This dog has" + dog.numLegs + ".";
+    return "This dog has " + dog.numLegs + " legs.";
   },
 };
 
