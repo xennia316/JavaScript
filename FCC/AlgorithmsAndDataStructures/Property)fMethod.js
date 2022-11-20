b6876055433c90201cb1c0e71c455be0f401f37b
xennia316 <xennia316@gmail.com>
2022-11-20 09:56:49 +0100

    hasOwnProperty of method

---
+++ FCC/AlgorithmsAndDataStructures/Property)fMethod.js
@@ -0,0 +1,12 @@
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+for (let property in canary) {
+  if (canary.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
