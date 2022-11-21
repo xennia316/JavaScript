a8bca986459666b0d36a507d285afa33e30adc7b
xennia316 <xennia316@gmail.com>
2022-11-21 11:27:31 +0100

    create an object with prototypes already defined

---
+++ FCC/AlgorithmsAndDataStructures/prototypeFunction.js
@@ -0,0 +1,14 @@
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+  numLegs: 4,
+  eat: function () {
+    console.log("nom nom nom");
+  },
+  describe: function () {
+    console.log("My name is " + this.name);
+  },
+};
